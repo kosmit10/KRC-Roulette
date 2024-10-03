9f4505da055c58641405365d0e255c97cf2656fe
kosmit10 <kosmit1001@gmail.com>
2024-10-03 12:56:18 +0200

    Initial Commit

---
+++ KRC Roulette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KRC Roulette
+//
+//  Created by maciura on 03/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KRC Roulette/KRC_RouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  KRC_RouletteApp.swift
+//  KRC Roulette
+//
+//  Created by maciura on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct KRC_RouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
