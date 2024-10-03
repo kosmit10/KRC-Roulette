645d35a2b39a1667ed6b248a370d2ff5f25a7590
kosmit10 <kosmit1001@gmail.com>
2024-10-03 17:47:31 +0200

    Add images logo, create ContentView(start_screen)

---
+++ KRC Roulette/Lottie/LottieView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    let animationName: String
+
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        let animationView = LottieAnimationView(name: animationName)
+        animationView.loopMode = .loop
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        view.addSubview(animationView)
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
