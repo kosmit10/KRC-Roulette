19c9fce651722e1a87a604ae7c22d169a15733c7
kosmit10 <kosmit1001@gmail.com>
2024-10-04 14:13:18 +0200

    Login Screen

--- KRC Roulette/Login_Screen/Login.swift
+++ KRC Roulette/Login_Screen/Login.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct Login: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("background_login")
+                .padding()
+            ZStack {
+                Image("Login-screen")
+                    .resizable()
+                    .frame(width: 370, height: 270)
+                VStack{
+                    VStack {
+                        ZStack{
+                            Image("pass-mail")
+                                .resizable()
+                                .frame(width: 180, height: 50)
+                        }
+                        ZStack{
+                            Image("pass-mail")
+                                .resizable()
+                                .frame(width: 180, height: 50)
+                        }
+                        
+                    }
+                    .padding(.leading, 120.0)
+                    .padding(.top, 110.0)
+                HStack{
+                    Button(action: {
+                        
+                    }){
+                        Image("button-login")
+                            .resizable()
+                            .frame(width: 150, height: 40)
+                        }
+                    Button(action: {
+                        
+                    }){
+                        Image("button-register")
+                            .resizable()
+                            .frame(width: 150, height: 40)
+                        }
+                    }
+                .padding(.bottom, 70.0)
+                }
+                .padding()
+                
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
