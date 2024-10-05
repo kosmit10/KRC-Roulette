1c8dee9f60c1971818bd6de73fd545e3b7b199d1
kosmit10 <kosmit1001@gmail.com>
2024-10-05 08:59:07 +0200

    Final Login(), add firebase Auth, Final Launch Screen (Content_View)

---
+++ KRC Roulette/Login_Screen/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  KRC Roulette
+//
+//  Created by maciura on 05/10/24.
+//
+
+import SwiftUI
+
+struct AppDelegate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDelegate()
+}
