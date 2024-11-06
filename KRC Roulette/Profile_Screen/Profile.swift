db1f197c6bb5954d5e4dc97561a56c7deb6c199b
kosmit10 <kosmit1001@gmail.com>
2024-11-06 12:23:27 +0100

    Final jackpot() View

---
+++ KRC Roulette/Profile_Screen/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  KRC Roulette
+//
+//  Created by maciura on 20/10/24.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
